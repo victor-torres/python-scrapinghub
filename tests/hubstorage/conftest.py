64c9ab7beff5ff1728818dae01f84177cb856cfe
Victor Torres <vpaivatorres@gmail.com>
2019-02-12 13:14:15 -0200

    make it possible to override the test project id and avoid committing private api keys replacing basic authentication headers

--- tests/hubstorage/conftest.py
+++ tests/hubstorage/conftest.py
@@ -16,7 +16,7 @@ from scrapinghub.hubstorage.serialization import MSGPACK_AVAILABLE
 from ..conftest import request_accept_header_matcher
 
 
-TEST_PROJECT_ID = "2222222"
+TEST_PROJECT_ID = os.environ.get('HS_PROJECT_ID', '2222222')
 TEST_SPIDER_NAME = 'hs-test-spider'
 TEST_FRONTIER_SLOT = 'site.com'
 TEST_BOTGROUP = 'python-hubstorage-test'
@@ -30,11 +30,31 @@ VCR_CASSETES_DIR = 'tests/hubstorage/cassetes'
 
 class VCRGzipSerializer(object):
     """Custom ZIP serializer for VCR.py."""
+    @staticmethod
+    def replace_basic_authentication_headers(cassette_dict):
+        """Replace Basic Authentication headers to prevent committing
+        private API Keys."""
+        interactions = []
+        for interaction in cassette_dict['interactions']:
+            if 'Authorization' in interaction['request']['headers']:
+                interaction['request']['headers'] = [
+                    'Basic {}'.format(
+                        base64.b64encode(
+                            '{}:'.format('f' * 32).encode('utf-8')
+                        )
+                    )
+                ]
+
+            interactions.append(interaction)
+
+        cassette_dict['interactions'] = interactions
+        return cassette_dict
 
     def serialize(self, cassette_dict):
         # receives a dict, must return a string
         # there can be binary data inside some of the requests,
         # so it's impossible to use json for serialization to string
+        cassette_dict = self.replace_basic_authentication_headers(cassette_dict)
         compressed = zlib.compress(pickle.dumps(cassette_dict, protocol=2))
         return base64.b64encode(compressed).decode('utf8')
 
